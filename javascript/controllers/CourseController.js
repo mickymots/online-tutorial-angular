5cfcaad405f0814b6921406788b1ff77a0bb3a56
Amit Prakash Singh <mickymots>
2016-01-30 17:48:53 +0530

    page level details

--- javascript/controllers/CourseController.js
+++ javascript/controllers/CourseController.js
@@ -9,7 +9,8 @@ var courseController = angular.module('courseController', []);
 	  
 	$scope.pageHeader = 'Module 1';
 	$scope.page="Content Loading..."
-    
+    $scope.pages = {};
+	
 	$scope.course = {"courseName":"Core Java", "modules": [{"id":1,"moduleName":"Module 1"},{"id":2,"moduleName":"Module 2"}, {"id":3,"moduleName":"Module 3"}, {"id":4,"moduleName":"Module 4"}]};
   
 	$scope.setModule = function(moduleId, moduleName){
@@ -19,8 +20,23 @@ var courseController = angular.module('courseController', []);
 		$log.info("calling xhr");
 		$http.get('resources/data/'+moduleId+'.json').success(function(data) {
 			$scope.page = data.data;
+			$scope.pages = data.pages;
 			
 		});
+		
+	};
+	
+	
+	$scope.setPage = function(pageId, pageName){
+		
+		$scope.pageHeader = pageName;
+		$scope.page = pageName;
+		$log.info("calling xhr");
+		// $http.get('resources/data/'+moduleId+'.json').success(function(data) {
+			// $scope.page = data.data;
+			// $scope.pages = data.pages;
+			
+		// });
 		
 	};
   
